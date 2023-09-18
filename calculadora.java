64e0ac242b8691a79c10f853cee0d9c372daf456
Clovis910 <clovis910@hotmail.com>
2023-09-18 14:28:27 -0300

    adicao e subtracao

---
+++ calculadora.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class calculadora {
+
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero: ");
+        double numero1 = sc.nextDouble();
+
+        System.out.println("Digite o segundo numero: ");
+        double numero2 = sc.nextDouble();
+
+
+        sc.close();
+
+        double soma= numero1 + numero2;
+        double subtracao = numero1 - numero2;
+
+
+        System.out.println("A soma é igual a: " + soma);
+        System.out.println("A subtracao é igual a: " + subtracao);
+
+
+
+
+    }
+    
+}
