1b0842f1d306a3e229034553496aef30acfcd2dd
Clovis910 <clovis910@hotmail.com>
2023-09-18 16:15:13 -0300

    modulo

--- calculadora.java
+++ calculadora.java
@@ -19,13 +19,13 @@ public class calculadora {
         double soma= numero1 + numero2;
         double subtracao = numero1 - numero2;
         double multiplicacao = numero1 * numero2;
-        double divisao = numero1 * numero2;
+        double modulo = numero1 % numero2;
 
 
         System.out.println("A soma é igual a: " + soma);
         System.out.println("A subtracao é igual a: " + subtracao);
         System.out.println(" A multiplicacao é igual a: " + multiplicacao);
-        System.out.println("A divisao é: " + + divisao);
+        System.out.println("O modulo é: " + + modulo);
 
 
 
